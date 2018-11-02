a184144b27026408f76dbdd2f435dab2011976dc
rudanlucena <rudanlucena@hotmail.com>
2018-11-02 15:24:01 -0300

    Usando padrao DTO para retotnar usuarios

--- src/main/java/com/arenamilgaru/workshopmongo/resources/UserResource.java
+++ src/main/java/com/arenamilgaru/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.arenamilgaru.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arenamilgaru.workshopmongo.domain.User;
+import com.arenamilgaru.workshopmongo.dto.UserDTO;
 import com.arenamilgaru.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> users = service.findAll();
-		return ResponseEntity.ok().body(users);
+		List<UserDTO> listDto = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
